e4e8d3f59f69e27168a031e37a1da96e03137f7a
Jedrzej Lewandowski <progreacja@gmail.com>
2018-05-12 21:51:14 +0200

    Update and Add packages, types, and scirpts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'focus-visible/dist/focus-visible.js'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
